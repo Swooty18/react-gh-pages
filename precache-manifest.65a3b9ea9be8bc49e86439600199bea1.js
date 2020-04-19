ed0cbc9e503c36450c7e49f745c8509efb415da3
Swooty18 <Ivan.matsiyk18092002@gmail.com>
2020-04-19 13:10:50 +0300

    Updates

---
+++ precache-manifest.65a3b9ea9be8bc49e86439600199bea1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1f54af4161fa7ec28d4e9a7fd574ea",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "a069c81924d5c278ce90",
+    "url": "/react-gh-pages/static/css/main.eb827a95.chunk.css"
+  },
+  {
+    "revision": "0b62638bdddc71806565",
+    "url": "/react-gh-pages/static/js/2.b545f7bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-gh-pages/static/js/2.b545f7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a069c81924d5c278ce90",
+    "url": "/react-gh-pages/static/js/main.175bbdea.chunk.js"
+  },
+  {
+    "revision": "0ae8e7d3039cc122bb3d",
+    "url": "/react-gh-pages/static/js/runtime-main.1710823f.js"
+  },
+  {
+    "revision": "04bce1bad57c42f2363ab204267bb4eb",
+    "url": "/react-gh-pages/static/media/add.04bce1ba.svg"
+  },
+  {
+    "revision": "10c581b6e3a0bc8ce7ac0527a423c2ad",
+    "url": "/react-gh-pages/static/media/close.10c581b6.svg"
+  },
+  {
+    "revision": "e6d3868a10f056f22a226b8cbe602bc9",
+    "url": "/react-gh-pages/static/media/edit.e6d3868a.svg"
+  },
+  {
+    "revision": "95f30ee9e97d62b2666b1f9c48e00ae7",
+    "url": "/react-gh-pages/static/media/remove.95f30ee9.svg"
+  }
+]);
